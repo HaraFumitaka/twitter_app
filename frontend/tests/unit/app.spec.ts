f4ed0ce6fdf129f07acd4d2e27d415c211e95852
HaraFumitaka <hara.fumitaka@brains-tech.co.jp>
2025-04-28 18:57:22 +0900

    fix:GitActions frontendのテストが動くように修正

--- frontend/tests/unit/app.spec.ts
+++ frontend/tests/unit/app.spec.ts
@@ -1,5 +1,15 @@
-describe('Placeholder Test', () => {
-  it('should pass', () => {
+import { TestBed } from '@angular/core/testing';
+
+describe('基本テスト', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+  });
+
+  it('テストが実行できること', () => {
     expect(true).toBeTruthy();
+  });
+  
+  it('数値の加算が正しく動作すること', () => {
+    expect(1 + 1).toBe(2);
   });
 });
